9d6f28dd7a938e47e3d4eca822a40c8809ff8435
sebastienfernandez <sebastienfernandez@mailo.com>
2019-09-11 15:25:57 +0200

    installation de bootstrap + début d'integration

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,56 @@
 <template>
-    <div>Bienvennue dans GeeCoock<br>Je me mets à compter, {{ start + number }}</div>
+    <div>
+        <header>
+            <h1>Avec la cantina, partagez vos recettes issues de la culture populaire !</h1>
+            <div class="md-form active-pink active-pink-2 mb-3 mt-0">
+                <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+            </div>
+        </header>
+        <main>
+            <section>
+                <h2>Filtrer vos recettes : </h2>
+                <div class="btn-group">
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Difficulté
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <button class="dropdown-item" href="#">Padawan</button>
+                            <button class="dropdown-item" href="#">Jedi</button>
+                            <button class="dropdown-item" href="#">Maître</button>
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Nombre de personnes
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <button class="dropdown-item" href="#">1-2</button>
+                            <button class="dropdown-item" href="#">3-4</button>
+                            <button class="dropdown-item" href="#">5-6</button>
+                            <button class="dropdown-item" href="#">7+</button>
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Temps de préparation
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <button class="dropdown-item" href="#">gg</button>
+                        <button class="dropdown-item" href="#">Another action</button>
+                        <button class="dropdown-item" href="#">Something else here</button>
+                    </div>
+                    </div>
+                </div>
+            </section>
+            <section></section>
+        </main>
+    </div>
 </template>
+
+<style>
+
+</style>
     
 
 
